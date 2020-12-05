251509debde08854d586a761ae4f97f3daf7418a
Philippe Turcotte <pturcotte@xorcube.com>
2020-12-05 23:39:36 +0100

    Removed navbar-menu component and fixed navbar collapse.

--- ceriquiz-spa/src/app/navbar/navbar.component.ts
+++ ceriquiz-spa/src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '../authentication/authentication.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  constructor() {
+  public isMenuCollapsed = true;
+  public isAuthenticated = false;
+
+  constructor(private authenticationService: AuthenticationService) {
   }
 
   ngOnInit(): void {
+    // S'enregistrer aux updates de l'état de l'authentification.
+    this.authenticationService.getAuthenticated().subscribe(value => {
+      this.isAuthenticated = value;
+    });
   }
 
 }
